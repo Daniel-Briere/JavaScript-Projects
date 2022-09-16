4e3b1f3b1616b95d049ffa790ee4c95221135709
Daniel-Briere <110870893+Daniel-Briere@users.noreply.github.com>
2022-09-16 01:36:20 -0600

    Fix project 2 added project 3

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -6,8 +6,8 @@ function My_First_Function() {              //Defining a function andnamming it
                                             //with the "Button_Text" id
 }
 
-function myFunction() {
+function my_Function() {
     var sentence = "I am learning";
-    sentence += "a lot from this book!";
+    sentence += " a lot from this book!";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
