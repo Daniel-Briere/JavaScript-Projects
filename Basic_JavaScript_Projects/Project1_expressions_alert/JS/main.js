3a895a37a436e2755da35d9312c61cc28526b9e0
Daniel-Briere <110870893+Daniel-Briere@users.noreply.github.com>
2022-09-16 00:41:58 -0600

    Basic JavaScript Projects Assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+document.write(3+3)
+
+function My_First_Function() {              //Defining a function andnamming it
+    var str = "This is the button text!";   //Defining a variable and giving it
+                                            //a string value
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value
+                                            //of the variable into the HTML element FromPoint
+                                            //with the "Button_Text" id
+}
